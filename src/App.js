b2ecc10871328e0263958d0ba8cc354515367e80
AimanaTazhibaeva <Aimasha200300@gmail.com>
2022-06-14 13:36:21 -0700

    Finished input pass a message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState }  from "react"
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const [inputTask, setInputTask] = useState("")
+   const [keepInput,setKeepInput] = useState([])
+   const [alert, setAlert] = useState(false)
+
+   const handleSubmit = () =>{
+     setKeepInput([...inputTask])
+      setInputTask("")
+
+      if(inputTask === ""){
+        setAlert(!alert)
+      }
+      
+      setTimeout(()=>{
+        setAlert(false)
+      },3000)
+
+   }
+   console.log(alert)
+   return(
+     <div className='App'>
+       <h1>A Message You Would Like To Pass</h1>
+
+       <input value={inputTask} onChange={(e)=>setInputTask(e.target.value)} type="text" />
+
+       <div>
+         <button onClick={handleSubmit}>Submit</button>
+         </div>
+         <p className={alert ? "alert-show": "alert-hide" }>Please Enter A Value To Pass</p>
+
+       <h2>Last Message Delivered</h2>
+
+       <p>{keepInput}</p>
+      
+     </div>
+   )
 }
 
 export default App;
+
+
